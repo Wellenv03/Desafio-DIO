3959f38441398806f4f28f66ed4778ff944bb8ce
Wellenv03 <149198211+Wellenv03@users.noreply.github.com>
2023-11-30 18:07:31 -0300

    Classificador de heróis
    Desafio proposto pela DIO

---
+++ index.js
@@ -0,0 +1,21 @@
+//saida
+console.log ("Digite o nome do seu jogador")
+
+//declara variavel
+let.nikname = "timtim do ajp"
+
+//concatenando uma mensagem fixa + varialvel
+const mensagem = "Bem vindo" + let.nikname
+
+//declarando operadores
+let.numero = "0, 1.000"
+console.log (nikname + numero)
+console.log (ferro)
+
+//declarando laços de repetiçao
+let pontosDoXP = 0; 1.000
+for(let i  = 0;  1.000 ; i++) { pontosDoXP += '.'.log ( "perdeu em batalha" + i)}
+
+// decarando estrutura de repetiçao
+let.XP = "nivelDoHeroi"
+switch (nivelDoHeroi) {case "ferro":} console.log(nivel)
